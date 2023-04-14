90b8e51000489a0bd914a4b1847a4c6a2b1fedd6
pouria@ifiveo.com <pouria@ifiveo.com>
2023-04-14 06:56:18 +0000

    changews

--- models/staging/int_stg_tim_quantity.sql
+++ models/staging/int_stg_tim_quantity.sql
@@ -1,43 +1,12 @@
-{{ config(materialized='ephemeral') }}
-with stg_talbot as(SELECT  *,
-        ISNULL(date, (SELECT TOP 1 date from {{ source('public','talbot_255') }} WHERE _row < t._row AND date IS NOT NULL ORDER BY _row DESC))
-  from {{ source('public','talbot_255') }}  t),
-  int_stg_talbot as (
-  select type,sandwich_sales_quantity,coffee_sales_quantity
-  ,drive_thru_time,coalesce as date,1642 as branch_id,'TH-ON-Leamington-1642-255-Talbot'as branch  from stg_talbot order by date ),
-  talbot_255 as (
-  select * from int_stg_talbot where type is not null order by date),
-  
-  
-  stg_maidstone as(SELECT  *,
-        ISNULL(date, (SELECT TOP 1 date from {{ source('public','maidstone_1903') }} WHERE _row < t._row AND date IS NOT NULL ORDER BY _row DESC))
-  from {{ source('public','maidstone_1903') }}  t),
-  int_stg_maidstone as (
-  select type,sandwich_sales_quantity,coffee_sales_quantity
-  ,drive_thru_time,coalesce as date,1903 as branch_id, 'TH-ON-Essex-1903-9-Maidstone'as branch  from stg_maidstone order by date ),
-  maidstone_1903 as (
-  select * from int_stg_maidstone where type is not null order by date),
-
-
-stg_erie185 as(SELECT  *,
-        ISNULL(date, (SELECT TOP 1 date from {{ source('public','erie_185') }} WHERE _row < t._row AND date IS NOT NULL ORDER BY _row DESC))
-  from {{ source('public','erie_185') }}  t),
-  int_stg_erie185 as (
-  select type,sandwich_sales_quantity,coffee_sales_quantity
-  ,drive_thru_time,coalesce as date, 562 as branch_id,'TH-ON-Leamington-562-185-Erie'as branch  from stg_erie185 order by date ),
-  erie185 as (
-  select * from int_stg_erie185 where type is not null order by date),
-
-  stg_erie250 as(SELECT  *,
-        ISNULL(date, (SELECT TOP 1 date from {{ source('public','erie_250') }} WHERE _row < t._row AND date IS NOT NULL ORDER BY _row DESC))
-  from {{ source('public','erie_250') }}  t),
-  int_stg_erie250 as (
-  select type,sandwich_sales_quantity,coffee_sales_quantity
-  ,drive_thru_time,coalesce as date,324 as branch_id, 'TH-ON-Leamington-324-250-Erie'as branch  from stg_erie250 order by date ),
-  erie250 as (
-  select * from int_stg_erie250 where type is not null order by date),
-
-  stg_union as (
+with talbot_255 as (SELECT  *,
+1642 as branch_id,'TH-ON-Leamington-1642-255-Talbot'as branch   from {{ source('public','talbot_255') }} where drive_thru_time is not null order by date ),
+ maidstone_1903 as (SELECT  *,
+1903 as branch_id,'TH-ON-Essex-1903-9-Maidstone' as branch   from {{ source('public','maidstone_1903') }} where drive_thru_time is not null order by date ),
+erie185 as (SELECT  *,
+562 as branch_id,'TH-ON-Leamington-562-185-Erie' as branch   from {{ source('public','erie_185') }} where drive_thru_time is not null order by date ),
+erie250 as (SELECT  *,
+324 as branch_id,'TH-ON-Leamington-324-250-Erie' as branch   from {{ source('public','erie_250') }} where drive_thru_time is not null order by date ),
+stg_union as (
         select * from talbot_255 
         UNION
         select *from maidstone_1903
@@ -46,7 +15,7 @@ stg_erie185 as(SELECT  *,
         UNION
         select * from erie250
   ),
-  stg_av_time as (
+stg_av_time as (
   select upper(type) as shift ,branch_id,sandwich_sales_quantity,coffee_sales_quantity
   ,drive_thru_time,date,branch,
 CASE WHEN shift='PM' then 35 
@@ -56,7 +25,7 @@ CASE WHEN shift='PM' then 35
      ELSE 0 END AS av_time
    from stg_union order by date desc),
 
-   score as(select  branch_id,shift ,sandwich_sales_quantity,coffee_sales_quantity
+score as(select  branch_id,shift ,sandwich_sales_quantity,coffee_sales_quantity
   ,drive_thru_time,av_time,CONVERT(datetime, date),branch,
   case when drive_thru_time<=22.5 and shift='WDAM' then 18
         when drive_thru_time>22.51 and drive_thru_time<25 and shift='WDAM' then 16
@@ -88,15 +57,11 @@ CASE WHEN shift='PM' then 35
         when drive_thru_time>50 and shift='PM' then 0
         ELSE -1 END AS customer_score  from stg_av_time),
 
-   sale as(select branch_id,shift ,sandwich_sales_quantity,coffee_sales_quantity
+sale as(select branch_id,shift ,sandwich_sales_quantity,coffee_sales_quantity
   ,drive_thru_time,av_time,date,EXTRACT(WEEK FROM date) as week ,branch,customer_score,EXTRACT(YEAR FROM date) AS year, 
     round(coffee_sales_quantity/av_time,2) as coffee_sale_per_hour,
     round(sandwich_sales_quantity/av_time,2) as sandwich_sale_per_hour from score where customer_score>-1 )
 
-select * from sale
+select * from sale  
 
   
-
-
-   
-   
